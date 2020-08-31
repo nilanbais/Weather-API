7b070cc620c5b9d23a2f1f27a0e7169888258f52
Bais <32563996+nilanbais@users.noreply.github.com>
2020-08-31 14:04:38 +0200

    Converted code to use of json instead of csv. Also extracting pd.Series with values instead of manually extracting data from response package.

--- weather api building block.py
+++ weather api building block.py
@@ -2,8 +2,11 @@
 Voor info API, zie:
 https://rapidapi.com/visual-crossing-corporation-visual-crossing-corporation-default/api/visual-crossing-weather/endpoints
 """
-import requests
 from datetime import datetime
+
+import pandas as pd
+import requests
+from pandas.io.json import json_normalize
 
 # De aan te passen variabelen in het standaard blok
 Datum = "01-01-2019"
@@ -17,7 +20,7 @@ url = 'https://visual-crossing-weather.p.rapidapi.com/history'
 
 # Definiëren van de querystring
 querystring = {"dayStartTime": "8:00:00",
-               "contentType": "csv",
+               "contentType": "json",
                "dayEndTime": "17:00:00",
                "shortColumnNames": "false",
                "startDateTime": f'{dateObject}T00:00:00',
@@ -32,20 +35,26 @@ headers = {'x-rapidapi-host': "visual-crossing-weather.p.rapidapi.com",
 
 # De API bevragen
 response = requests.request("GET", url, headers=headers, params=querystring)
+r = response.json()
+df = json_normalize(r)
 
-# De response van de API manipuleren (kolommen en waarden splitsen)
-r = response.text.split('\n')
-par_names = r[0].split(',')
-par_val = r[1].replace('"', '').split(',')
+# Isoleren van de pd.Series met de waarden
+df2 = df[f'locations.{Locatie.capitalize()}%2C NL.values']
 
-# De kolomnamen aan de juiste waarden kopellen
-weerdata = dict()
-for i in range(len(par_names)):
-    weerdata[par_names[i]] = par_val[i]
+# Isoleren van de dict met waarden uit de pd.Series
+_d = df2.values
+d = _d[0][0]
 
-# Statische variabele voor in de {} van de onderstaande f-string
-datum = 'Date time'
-temperatuur = 'Temperature'
+# Definiëren van de gewenste waarden
+temp = d['temp']
+mint = d['mint']
+maxt = d['maxt']
+humidity = d['humidity']
+condition = d['conditions']
 
-# Printen van een resultaat
-print(f'Op {weerdata[datum]} was het {weerdata[temperatuur]} graden Celsius in {Locatie}')
+# Genereren van één dataframe met de gewenste waarden
+weerdata = pd.DataFrame([{temp, mint, maxt, humidity, condition}],
+                        columns=['temp', 'min temp', 'max temp', 'humidity', 'condition'])
+
+# Printen om het resultaat te presenteren
+print(weerdata)
